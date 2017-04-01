9c008fba61b2c4139b5b3a29cba7858a64099adf
Lee Pender <lpender@gmail.com>
2017-04-01 15:21:51 -0400

    Bump 2.4.1 -> 2.5.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'lpender:pushwoosh',
   summary: 'Send and receive push notifications via Pushwoosh',
-  version: '2.4.1',
+  version: '2.5.0',
   git: 'https://github.com/lpender/meteor-pushwoosh.git'
 });
 
